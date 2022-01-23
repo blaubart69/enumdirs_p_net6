c83fbdde74fa970ced259ca2b51465750ce8651e
Bernhard Spindler <bernhard.spindler.75@gmail.com>
2022-01-23 10:09:56 +0100

    initial

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Spi;
+
+internal class Program
+{
+    static void enumdir_p(string startPath)
+    {
+        var paths = System.Threading.Channels.Channel.CreateUnbounded<string>();
+
+        paths.Writer.TryWrite(startPath);
+        long count = 1;
+
+        for (int i = 0; i < 15; ++i)
+        {
+            Task.Run(async () =>
+            {
+               for (; ; )
+               {
+                   string path = await paths.Reader.ReadAsync();
+                   foreach (string filename in System.IO.Directory.EnumerateFileSystemEntries(path, "*"
+                   , new EnumerationOptions()
+                   {
+                       RecurseSubdirectories = false,
+                       ReturnSpecialDirectories = false,
+                       AttributesToSkip = 0
+                   }))
+                   {
+                       var fi = new FileInfo(filename);
+                       if (fi.Attributes.HasFlag(FileAttributes.Directory))
+                       {
+                           Interlocked.Increment(ref count);
+                           paths.Writer.TryWrite(filename);
+                           continue;
+                       }
+                       Console.WriteLine($"{fi.Length,12:n0}\t{filename}");
+                   }
+                   if (Interlocked.Decrement(ref count) == 0)
+                   {
+                       paths.Writer.Complete();
+                   }
+               }
+           });
+        }
+        paths.Reader.Completion.Wait();
+    }
+    static void Main(string[] args)
+    {
+        enumdir_p(".");
+    }
+}
